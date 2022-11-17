a26a1b05c5f7a3ecd102ddb89071709ce5f2e456
Tim <macgarry@insa-toulouse.fr>
2022-11-17 11:58:34 +0100

    ActiveUserManager3

--- src/main/java/Managers/ActiveUserManager.java
+++ src/main/java/Managers/ActiveUserManager.java
@@ -1,13 +1,18 @@
 package Managers;
 import Models.User;
+import java.util.ArrayList;
 
 
 public class ActiveUserManager {
-    public ActiveUserManager(U : User){
-        this.User = User;
+    public ActiveUserManager(User U){
+        this.User = U;
+        ArrayList<User> listActiveUsers = new ArrayList<User>();
     }
-    public updateListActiveUser(U : User ){
+    void addListActiveUser(User U){
+        listActiveUsers.add(U);
+    }
 
+    void removeListActiveUser(User U){
+        listActiveUsers.remove(U);
     }
-    return 0;
 }
