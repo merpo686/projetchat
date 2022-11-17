7860488ff3b768ad6bfded8cba25ae6f32197a42
Poitou Merlin <poitou@insa-20551>
2022-11-17 12:07:39 +0100

    classes5

---
+++ src/main/java/Managers/NetworkManager.java
@@ -0,0 +1,12 @@
+package Managers;
+
+import Models.*;
+
+public class NetworkManager {
+    int Port;
+    public NetworkManager(int port){
+        this.Port=port;
+    }
+    public void NotifyPseudo(Notif notif){
+    }
+}
--- src/main/java/Managers/UserManager.java
+++ src/main/java/Managers/UserManager.java
@@ -1,4 +1,25 @@
 package Managers;
 
+import Models.*;
+
+import java.util.Scanner;
+
 public class UserManager {
+    String Pseudo;
+    NetworkManager NM;
+    public UserManager(String Pseudo){
+        this.Pseudo=Pseudo;
+        this.NM=new NetworkManager(5000);
+    }
+    public void Connect(){
+        String pseudochoosed=null;
+        pseudochoosed=Ask_Pseudo();
+        Notif notifpseudo = new Notif(3,pseudochoosed); //type3 = notif sert à r pour l'instant
+        NM.NotifyPseudo(notifpseudo);
+    }
+    private String Ask_Pseudo(){
+        Scanner sc = new Scanner(System.in);
+        String ps = sc.nextLine();
+        return ps;
+    }
 }
