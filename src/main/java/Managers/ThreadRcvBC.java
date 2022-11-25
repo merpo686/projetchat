47a64b540551993b33e5e1e9f73eac776958c688
Tim <macgarry@insa-toulouse.fr>
2022-11-25 17:16:30 +0100

    Sprint -v15

--- src/main/java/Managers/ThreadRcvBC.java
+++ src/main/java/Managers/ThreadRcvBC.java
@@ -17,32 +17,29 @@ public class ThreadRcvBC implements Runnable {
         try {
             byte[] data = new byte[1024];
             String rcvData;
-            DatagramSocket socket = new DatagramSocket(4567);
+            DatagramSocket socket = new DatagramSocket(UserManager.user_self.get_Port());
             DatagramPacket rcvNotif = new DatagramPacket(data, data.length);
             //we receive the model (either notification or validation)
             while (true) { //the thread will be in a receiving state constantly
                 try {
                     System.out.println("[ThreadRcvBC] Waiting for Broadcast");
                     socket.receive(rcvNotif);
+                    System.out.println("[ThreadRcvBC]"+rcvNotif.getAddress().getHostAddress());
+                    System.out.println(UserManager.user_self.get_Hostname());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
                 if (rcvNotif.getLength() == 0) {
                     System.out.println("[ThreadRcvBC] Read zero bytes");
-                } else if (rcvNotif.getAddress().getHostName() == UserManager.user_self.get_Hostname()) {
-                    System.out.println("Broadcasted to myself");
-                } else if (rcvNotif.getAddress().getHostName() == UserManager.user_self.get_Hostname()) {
-                    System.out.println("[ThreadRcvBC] Broadcasted to myself");
-                } else {
+                } else if (!(rcvNotif.getAddress().getHostName() == UserManager.user_self.get_Hostname())){
                     rcvData = new String(rcvNotif.getData());
                     //we need to check if notification or validation... we split the string that we received with "-" as a delimiter. If we only have 1 element then that means it's a notification because it does not contain the boolean, if we have 2 elements that means it's a validation
                     String[] splitString = rcvData.split("-");
                     if (splitString.length == 1) {
-                        this.notif = new Notifications(new User(rcvNotif.getAddress().getHostName(), socket.getPort()), splitString[0]);
+                        this.notif = new Notifications(new User(rcvNotif.getAddress().getHostName(), UserManager.user_self.get_Port()), splitString[0]);
                         NM.Receive_BC(this.notif);
                     } else if (splitString.length == 2) {
-                        this.valid = new Validation(new User(rcvNotif.getAddress().getHostName(), socket.getPort()), splitString[0], Boolean.parseBoolean(splitString[1]));
+                        this.valid = new Validation(new User(rcvNotif.getAddress().getHostName(), UserManager.user_self.get_Port()), splitString[0], Boolean.parseBoolean(splitString[1]));
                         NM.Receive_BC(this.valid);
                     } else {
                         System.out.println("[ThreadRcvBC] Error there are 3 or more separate fields in the broadcast message");
